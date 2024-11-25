908caef5fa383c0ba538d1b7265a568902ef67e9
mustafa <adhdstudios42@gmail.com>
2024-11-25 18:16:52 +0500

    Update mainwindow.h

--- src/GUI/mainwindow.h
+++ src/GUI/mainwindow.h
@@ -1,12 +1,27 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-class MainWindow {
-public:
-    MainWindow();
-    ~MainWindow();
+#include <QMainWindow>
+#include <QTabWidget>
+#include <QTextEdit>
+#include <QWidget>
 
-    void show();
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow();         // Constructor
+    ~MainWindow();        // Destructor
+
+    void show();          // Show the main window
+
+private slots:
+    void addNewTab();     // Slot to add a new tab
+    void closeTab(int index); // Slot to close a specific tab
+
+private:
+    QWidget *centralWidget; // Main central widget
+    QTabWidget *tabWidget;  // Tab widget for managing multiple tabs
 };
 
 #endif // MAINWINDOW_H
